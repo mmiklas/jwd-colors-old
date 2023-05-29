1fdd8f9b0234ed36f9fa9cdc2c9bc658bea76ff2
Marcin Mikłas <miki@wszib.edu.pl>
2023-05-29 22:35:51 +0200

    Konfiguracja bazy danych

--- src/main/java/pl/edu/wszib/jwd/colors23/Colors23Application.java
+++ src/main/java/pl/edu/wszib/jwd/colors23/Colors23Application.java
@@ -2,7 +2,9 @@ package pl.edu.wszib.jwd.colors23;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class Colors23Application {
 
