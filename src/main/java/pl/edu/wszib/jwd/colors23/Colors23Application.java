b99c880d2ad8ae62ef2b3d0b6b7bc2aedb285c4e
Marcin Mikłas <miki@wszib.edu.pl>
2023-05-29 22:03:31 +0200

    Startowy commit

---
+++ src/main/java/pl/edu/wszib/jwd/colors23/Colors23Application.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.colors23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Colors23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Colors23Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wszib/jwd/colors23/Colors23ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.jwd.colors23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Colors23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
